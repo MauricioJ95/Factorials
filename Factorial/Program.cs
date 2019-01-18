d8646492aface06d352fd14cc21d869ea0121c73
Mauricio Reyna <mauriciojreyna@gmail.com>
2019-01-18 07:26:09 -0500

    final

---
+++ Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Please input a number between 1-10");
+                int num = int.Parse(Console.ReadLine());
+
+                long factorial = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+
+                Console.WriteLine("The factorial of {0} is {1}", num, factorial);
+                Console.ReadKey();
+
+
+                Console.WriteLine("Continue? Y/N)");
+                input = Console.ReadLine();
+
+            }
+            while (input == "y");
+        }
+    }
+}
+// :) Have a great day!
